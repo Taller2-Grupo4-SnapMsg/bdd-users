86d95bf1ed2dff14c4d4dc9f6589b766badd9aa3
lara-scazzola1 <lscazzola@fi.uba.ar>
2023-09-08 13:00:50 -0300

    saco el archivo database.py y lo que tenia lo paso a main

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
-# prueba-fly.io
+# Prueba base de datos usuario
 
 Para hacer una migración de la base de datos:
 
+```
 alembic revision --autogenerate -m "motivo de la migración"
+```
 
 Para aplicar la migracion:
 
+```
 alembic upgrade head
+```
+
+Para hacer las consultas o modificaciones en models/users/queries.py 
+hay algunas funciones que se importaron al main y se pueden usar desde ahi, 
+esta todo comentado salvo la que imprime las filas de la tabla
+
+```
+python3 main.py
+```
 
 
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 
 from sqlalchemy import create_engine
-from sqlalchemy.sql import text
+from sqlalchemy.orm import sessionmaker
+from models.users.model import User
+from models.users.queries import *
+from models.users.model import Base
 
+#Esto es lo que iria en el backend para conectarme y tendria que ver como importar las funciones para utilizar la bdd
+
+# Crear una conexión a la base de datos
 engine = create_engine("postgresql://cwfvbvxl:jtsNDRjbVqGeBgYcYvxGps3LLlX_t-P5@berry.db.elephantsql.com:5432/cwfvbvxl")
 
+# Crea las tablas en la base de datos (esto creará todas las tablas definidas en tus modelos)
+Base.metadata.create_all(engine)
 
-def app():
-    with engine.connect() as conn:
-        stmt = text("select * from pg_database")
-        #print(conn.execute(stmt).fetchall())
+# Crear una sesión para interactuar con la base de datos
+Session = sessionmaker(bind=engine)
+session = Session()
 
+#new_user = create_user(session, "marta00", "#pass00", "marta00@fi.uba.ar")
+#new_user = create_user(session, "marta01", "#pass01", "marta01@fi.uba.ar")
+#new_user = create_user(session, "marta02", "#pass02", "marta02@fi.uba.ar")
+#new_user = create_user(session, "marta03", "#pass03", "marta03@fi.uba.ar")
+#user_id = get_id_by_username(session, "marta02")
+#delete_user(session, user_id)
 
-if __name__ == "__main__":
-    app()
+users = session.query(User).all()
+for user in users:
+    print(f"ID: {user.id}, Username: {user.username}, Password: {user.password}, Email: {user.email}")
+
+session.close()
+
 
